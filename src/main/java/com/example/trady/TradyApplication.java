9111c1e29ce04a106021ea35d65d872e83a20c73
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2024-11-20 21:02:57 +0900

    first commit

---
+++ src/main/java/com/example/trady/TradyApplication.java
@@ -0,0 +1,13 @@
+package com.example.trady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradyApplication.class, args);
+    }
+
+}

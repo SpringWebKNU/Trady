e753a957bd8fab381df36ad3d160aa32440281f4
LeeTheY <peter20011148@gmail.com>
2024-11-25 20:16:56 +0900

    dodo_up

---
+++ src/main/java/com/example/trady/SessionConst.java
@@ -0,0 +1,6 @@
+package com.example.trady;
+
+public class SessionConst {
+
+    public static final String LOGIN_MEMBER = "loginMember"; // 로그인 회원 정보
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,9 +6,9 @@ VALUES
     ('Product4', '400000', 'shoes4.jpg'),
     ('Product5', '500000', 'shoes5.jpg');
 
-INSERT INTO member(username, password, email)
+INSERT INTO member(username, password, email, phonenumber, addr)
 VALUES
-    ('dododo','123','123@naver.com'),
-    ('fofofo','456','456@naver.com'),
-    ('gogogo','789','789@naver.com'),
-    ('hohoho','012','012@naver.com');
+    ('dododo','123','123@naver.com', '010-1234-5678', 'gangnam'),
+    ('fofofo','456','456@naver.com', '010-9876-5432', 'yongin'),
+    ('gogogo','789','789@naver.com', '010-1928-3746', 'suwon'),
+    ('hohoho','012','012@naver.com', '010-5473-3245', 'seoul');
